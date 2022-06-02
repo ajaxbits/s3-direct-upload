0e40971ef3b2e2817ff9b95cef75b298a7a8fd73
Alex Jackson <ajaxbits@pm.me>
2022-06-02 20:56:02 +0000

    add lambda handler and rename to app.js

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+function loadHTML() {
+  fetch("./index.html")
+    .then((response) => response.text());
+}
+
+exports.handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    body: loadHTML(),
+  };
+  return response;
+};
